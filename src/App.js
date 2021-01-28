83095cc02a0ca16e1ebffb84ffe7d78c3c773ebc
Julian Torp <julian.torp@nav.no>
2021-01-28 21:23:38 +0100

    initial commit
    Co-authored-by: Polkagris <arnenbj@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <div>
+                    <a href="https://redeyemedia.io" target="_blank" rel="noreferrer">
+                        <img src={logo} className="logo" alt="logo"/>
+                    </a>
+                </div>
+                <div className="headerContainer">
+                    <h1 className="title">Welcome to Red Eye Media</h1>
+                    <span className="motto">- Helping you help yourself ;)</span>
+                </div>
+                <div>
+                    <p>At Red Eye Media we want you to learn and improve in order to become a successful developer.</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
