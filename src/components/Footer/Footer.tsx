83dedfc847c4b9e58698b8b7c9d694b07b321a83
jjtg <julian.torp@gmail.com>
2021-02-19 22:52:20 +0100

    remove unused import

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HStack, Button } from "@chakra-ui/react";
-import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
